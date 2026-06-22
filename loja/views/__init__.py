03cc898a12afbb41a25263935c41dc7d3e3647b0
Joao <feitosa.joao@escolar.ifrn.edu.br>
2026-06-22 10:57:12 -0300

    commit

--- loja/views/__init__.py
+++ loja/views/__init__.py
@@ -1,4 +1,4 @@
 from .HomeView import home_view
-from .ProdutoView import list_produto_view
+from .ProdutoView import list_produto_view, create_produto_view, edit_produto_view, edit_produto_postback
 
-__all__ = ['home_view', 'list_produto_view']
+__all__ = ['home_view', 'list_produto_view', 'create_produto_view', 'edit_produto_view', 'edit_produto_postback']
